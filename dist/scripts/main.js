bb1887618a145423ede08adc754347088a18b89a
Zach Chaney <zachary.chaney@gmail.com>
2015-05-30 19:40:14 -0500

    made changes blah, blah, blah

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -4,6 +4,8 @@ $('.album-containers a').on('click', clickToView);
 
 function clickToView() {
 	$('.album1').show();
+	$('.album-containers').hide();
+
 	}
 // var myInterval = setInterval(function () {
 // 	console.log(new Date());
