0639152e61825ae214bf26b920e188ff4f6cb0b7
Zach Chaney <zachary.chaney@gmail.com>
2015-05-30 23:45:47 -0500

    trying to fix this POS

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,17 +1,52 @@
 $(document).ready(function () {
 
-$('.album-containers a').on('click', clickToView);
+// $('.album-containers a').on('click', clickToView);
 
-function clickToView() {
-	$('.album1-photos').show();
-	$('.albums-nav').show();
-	$('.album-containers').hide();
+// function clickToView() {
+// 	$('.album1-photos').show();
+// 	$('.albums-nav').show();
+// 	$('.album-containers').hide();
+// 	}
+
+
+var targetSection = $(window.location.hash);
+	if(!targetSection.length) {
+		targetSection = $(".albums album1-photos").eq(0);
 	}
+	targetSection.show();
 
-function clickToAlbum() {
-	$('.album1').show();
-	$('.album1-photos').show();
-	$('.album-containers').hide();
-}
+$('.album-containers a').on('click', function(e) {
+		var target = $(e.target);
+		var targetName = target.attr('href');
+		$('.album-containers').hide();
+		$(targetName).show();
+	});
+
+// $('.albums-nav').on('click', function(e) {
+// 		var $target = $(e.target);
+// 		var targetName = $target.attr('href');
+// 		$('albums-containers').hide();
+// 		$(targetName).show();
+// 	});
+
+
+// function clickToAlbum() {
+// 	$('.album2').show();
+// 	$('.album2-photos').show();
+// 	$('.album-containers').hide();
+// }
+
+// $('.albums-nav a').click(function(e) {
+// 	var target = $(e.target);
+// 	if(target.is("a")) {
+// 		target.show();
+// 	}
+// });
+
+
+// $('#album1-photos').click(function(e) {
+// 	var target = $(e.target);
+
+// })
 
 });
