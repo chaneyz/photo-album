936eeecd8484dbfab27a33c52b9200a210ed1f49
Zach Chaney <zachary.chaney@gmail.com>
2015-05-30 21:22:44 -0500

    commiting current updates

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -3,13 +3,15 @@ $(document).ready(function () {
 $('.album-containers a').on('click', clickToView);
 
 function clickToView() {
-	$('.album1').show();
+	$('.album1-photos').show();
+	$('.albums-nav').show();
 	$('.album-containers').hide();
-
 	}
-// var myInterval = setInterval(function () {
-// 	console.log(new Date());
-// }, 1000)
 
+function clickToAlbum() {
+	$('.album1').show();
+	$('.album1-photos').show();
+	$('.album-containers').hide();
+}
 
 });
