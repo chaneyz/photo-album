f57e21d8a8762cf898283a446341eef6953730e8
Zach Chaney <zachary.chaney@gmail.com>
2015-05-30 19:34:04 -0500

    adding .show function

---
+++ dist/scripts/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+$('.album-containers a').on('click', clickToView);
+
+function clickToView() {
+	$('.album1').show();
+	}
+// var myInterval = setInterval(function () {
+// 	console.log(new Date());
+// }, 1000)
+
+
+});
