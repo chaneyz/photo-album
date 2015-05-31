bd25e913652b4c3df22efd3cb5e1f265d01f3add
Zach Chaney <zachary.chaney@gmail.com>
2015-05-31 02:12:22 -0500

    this is driving me mad

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,25 +1,33 @@
 $(document).ready(function () {
 
-// $('.album-containers a').on('click', clickToView);
-
-// function clickToView() {
-// 	$('.album1-photos').show();
-// 	$('.albums-nav').show();
-// 	$('.album-containers').hide();
-// 	}
-
-
 // var targetSection = $(window.location.hash);
 // 	if(!targetSection.length) {
 // 		targetSection = $(".albums album1-photos").eq(0);
 // 	}
 // 	targetSection.show();
 
+
 $(".album-containers a").on("click", function(e) {
 		var target = $(this);
 		var targetName = target.attr("href");
+		
 		$('.album-containers').hide();
 		$(targetName).show();
 		$('.albums-nav').show();
 	});
+
+
+
+$("#albums-nav a").on("click", function(e) {
+		var target = $(this);
+		var targetName = target.attr("href");
+
+		$(targetName).show();
+		$('.album-containers').hide();
+		$('.albums-nav').show();
+
+	});
+
+
+
 });
