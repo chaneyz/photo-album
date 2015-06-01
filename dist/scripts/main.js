8c214d24733ac0b5d42cea1d5eec492606a9cf69
Zach Chaney <zachary.chaney@gmail.com>
2015-06-01 08:56:07 -0500

    final for hw round 1

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,12 +1,5 @@
 $(document).ready(function () {
 
-// var targetSection = $(window.location.hash);
-// 	if(!targetSection.length) {
-// 		targetSection = $(".albums album1-photos").eq(0);
-// 	}
-// 	targetSection.show();
-
-
 $(".album-containers a").on("click", function(e) {
 		var target = $(this);
 		var targetName = target.attr("href");
@@ -17,29 +10,25 @@ $(".album-containers a").on("click", function(e) {
 	});
 
 $(".albums-nav a").on("click", function(e) {
-  // hide all `.album`
-  $(".albums [id^=album]").hide();
-  // show `target` `.album`
-  $($(this).attr("href")).show();
-});
+		// hide all `.album`
+		$(".albums [id^=album]").hide();
+		// show `target` `.album`
+		$($(this).attr("href")).show();
+	});
 
-// $(".albums-nav a").on("click", function(e) {
-// 		var target = $(this);
-// 		var targetName = target.attr("href");
-
-// 		$(target).addClass("active");
-		
-// 		if($(target).hasClass("active")) {
-// 			$(targetName).show();
-// 		}
-
-// 		else {
-// 			$(".albums").hide();
-// 		}
-
-// 		$('.album-containers').hide();
-// 		$('.albums-nav').show();
+// $("#title").on("click", function(e) {
+// 	$(".album-containers").show();
+// 	$(".sidemenu").hide();
 
 // 	});
+
+// $(".albums-nav a").on("click", function(e) {
+// 		// hide all `.album`
+// 		$(".albums [id^=album]").hide();
+// 		// show `target` `.album`
+// 		$($(this).attr("href")).show();
+// 	});
+
+
 
 });
